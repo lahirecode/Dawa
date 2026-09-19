8e34b66817bec196d5b836513f86ec966b67d624
G.E.L <lahirekings@gmail.com>
2026-09-19 11:47:22 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,7 +1,29 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = java.io.File(rootDir, "local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
+        }
+        val flutterSdk = properties.getProperty("flutter.sdk")
+        require(flutterSdk != null) { "flutter.sdk not set in local.properties" }
+        flutterSdk
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
-include ":app"
+
+include(":app")
